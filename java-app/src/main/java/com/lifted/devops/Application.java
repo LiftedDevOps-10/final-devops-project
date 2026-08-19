8714e0570431a6737c0d16e723bad097c2a4eb06
LiftedDevOps-10 <liftedbymercy24@gmail.com>
2026-08-19 12:02:27 +0100

    Build containerized Java Maven application

---
+++ java-app/src/main/java/com/lifted/devops/Application.java
@@ -0,0 +1,23 @@
+package com.lifted.devops;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+            "======================================"
+        );
+
+        System.out.println(
+            "   Lifted DevOps Java Application"
+        );
+
+        System.out.println(
+            "   Application started successfully!"
+        );
+
+        System.out.println(
+            "======================================"
+        );
+    }
+}
